e857aa68a7e705d559c34fa9b44057df834e0eda
Nishanth <kananth2018@gmail.com>
2025-01-23 19:36:35 -0500

    Initial commit

--- hobby_hive/settings.py
+++ hobby_hive/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -107,9 +109,9 @@ WSGI_APPLICATION = 'hobby_hive.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_db_4',
+        'NAME': 'my_db_1',
         'USER': 'postgres',
-        'PASSWORD': 'root',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
